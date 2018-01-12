23902086a32bf7630c8ab4e586f39b64ec21573f
Leandro Roser <learoser@gmail.com>
2018-01-12 02:32:58 -0300

    .

--- R/ZZZ.R
+++ R/ZZZ.R
@@ -4,3 +4,15 @@
 .onUnload <- function (libpath) {
   library.dynam.unload("chunkR", libpath)
 }
+
+.onAttach <- function(...) {
+  
+  vers <- utils::packageDescription("chunkR", fields = "Version")
+  
+  textstart<- paste("\n", " <-- chunkR --<", "\n\n",
+                    "  Version",  vers, "\n",
+                    "  GitHub: chunkR_devel()", "\n",
+                    "  Comments / suggestions / bug reports: learoser@gmail.com \n")
+  
+  packageStartupMessage(textstart)
+}
---
+++ R/auxiliar.R
@@ -0,0 +1,10 @@
+
+#' chunkR devel
+#' @description The function opens the chunkR-devel web site:
+#' https://github.com/leandroroser/chunkR
+#' @export
+
+chunkR_devel <- function(){
+  cat("Opening link: https://leandroroser.github.io/chunkR\n")
+  browseURL("https://github.com/leandroroser/chunkR")
+}
